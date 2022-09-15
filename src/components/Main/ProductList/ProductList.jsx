7e5eca7170e7bf0a2e1e9451c751e1b3d1250775
Alejandro Reyes <reyesbalejandroe@gmail.com>
2022-09-15 13:49:46 +0200

    add/setState

--- src/components/Main/ProductList/ProductList.jsx
+++ src/components/Main/ProductList/ProductList.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 import ProductItem from './ProductItem'
+import data from './products.json'
 
 export class ProductList extends Component {
 
@@ -9,17 +10,30 @@ export class ProductList extends Component {
     super(props)
   
     this.state = {
-      products:[
-        {name:"Tigre de Bengala",info:"botella Moet con Bengala",price:20},
-        {name:"Corona party",info:"Cubo de 5 coronitas",price:10},
-        {info:"Botella de absenta con agua",price:40}
-      ]
+      products:data // precarga de [] de productos
     }
     //this.paintProducts = this.paintProducts.bind(this);
   }
 
   // paintProducts () {return this.state.products.map(product=><ProductItem data={product}/>)} 
-  paintProducts = () => this.state.products.map((product,i)=><ProductItem data={product} key={uuidv4()}/>)
+  paintProducts = () => this.state.products.map((product,i)=><ProductItem data={product} key={uuidv4()} delete={()=> this.deleteProduct(i)}/>)
+
+  deleteProducts = () => this.setState({products:[]});
+  resetProducts = () => this.setState({products:data}); //recarga inicial
+
+  deleteProduct = (i) => {
+    //filter
+    const remainingProducts = this.state.products.filter((product,j)=>i!==j);
+    this.setState({products:remainingProducts});
+  }
+  
+  addProduct = () => {
+    const name = prompt("Introduce nombre");
+    const info = prompt("Introduce info del producto");
+    const price = prompt("Introduce precio");
+    const newProduct = {name,info,price};
+    this.setState({products:[...this.state.products,newProduct]}); // [{},{},{}] ---> [{},{},{},{newProduct}]
+  }
 
   render() {
     
@@ -30,7 +44,16 @@ export class ProductList extends Component {
     // ]
     return (
       <section>
+            <h1>Productos para la fiesta</h1>
             {this.paintProducts()}
+
+            {this.state.products.length?
+            <button onClick={this.deleteProducts}>Borrar productos</button>
+            :""}
+
+            <button onClick={this.addProduct}>Añadir producto</button>
+            <button onClick={this.resetProducts}>Recargar productos</button>
+            
             {/* {products.map(product=><ProductItem data={product}/>)} */}
 
             {/* <ProductItem data={products[0]}/>
