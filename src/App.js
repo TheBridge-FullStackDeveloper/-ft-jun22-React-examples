b00ba433b5cd5cdf850442eb690d14355a6f144b
Guille-Rubio <guillermorubiog@gmail.com>
2022-09-16 11:41:00 +0200

    ADD/ Routing and basic components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
